9f84ce91064ac22f70c7ef99b21bcdeb0add0145
hyoon <hailey@poietes.art>
2019-05-24 18:55:16 -0400

    style: update gitRanks header

--- src/components/gitRanks/Header.js
+++ src/components/gitRanks/Header.js
@@ -6,11 +6,18 @@ const LangNav = props => {
       <header className="text-align-center">
         <h1 className="h2 pt-5">Popular Repos by Languages</h1>
         <div className="row justify-content-center">
-          <p className="col-12 col-md-6">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
-            sapiente quibusdam, tenetur officia quam facilis magnam earum nobis
-            quaerat? Sint?
+          <p className="col-12 col-md-6 mb-0">
+            Select language, and see the popular repos on Github.com
+            <br />
+            Data comes from Github API.
           </p>
+          <a
+            className="text-sm p-2 word-break-all line-height-sm link-hover-teal"
+            href="https://api.github.com/search/repositories?q=stars:>1+language:all&sort=stars&order=desc&type=Repositories"
+            target="_blank"
+          >
+            "https://api.github.com/search/repositories?q=stars:>1+language:all&sort=stars&order=desc&type=Repositories"
+          </a>
         </div>
       </header>
     </div>
